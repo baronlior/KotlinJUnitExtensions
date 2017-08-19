1e6f0e60282b7b2084eeb262f5f9900032a05dda
Lior Bar-On <baronlior@gmail.com>
2017-08-19 20:11:55 +0300

    add matchers: toMatchRegex toBeDeepEqual

--- src/main/kotlin/com/baronlior/junit4/Assert.kt
+++ src/main/kotlin/com/baronlior/junit4/Assert.kt
@@ -21,7 +21,15 @@ class Matcher(private val expectation: String) {
 
   infix fun Any.toBe(actual: Any) = Assert.assertEquals(e, this, actual)
   infix fun Any.notToBe(actual: Any) = assert(this != actual, "$e expected: not equal but was: both <$actual>")
-  infix fun Int.toBeLessThan(actual : Int) = assert(this < actual, "$e expected: <$this> to be less than <$actual>")
+  // toBeSame - ===
+
+  infix fun <T> Array<out T>.toBeDeepEqual(actual: Array<out T>) =
+      assert(this contentDeepEquals actual, format(e, this.contentDeepToString(), actual.contentDeepToString()))
+
+  infix fun String.toMatch(regex: String) =
+      assert(regex.toRegex().find(this) != null, "$e expected: <$this> to match regex <$regex>")
+
+  infix fun Int.toBeLessThan(actual : Int) = assert(this < actual, "$e expected: <$this> to be less than <$actual>") // TODO: support generic number: https://stackoverflow.com/questions/40587118/kotlin-generic-operation-on-number
 
 }
 
@@ -43,3 +51,20 @@ class Argument(private val text: String) : Assert() {
 
 }
 
+internal fun format(message: String, expected: Any, actual: Any): String {
+  val formatted = message + " "
+
+  val expectedString = expected.toString()
+  val actualString = actual.toString()
+
+  return if (expectedString == actualString) {
+    "${formatted}expected: ${formatClassAndValue(expected, expectedString)} but was: ${formatClassAndValue(actual, actualString)}"
+  } else {
+    "${formatted}expected: <$expectedString> but was: <$actualString>"
+  }
+}
+
+private fun formatClassAndValue(value: Any?, valueString: String): String {
+  val className = if (value == null) "null" else value.javaClass.name
+  return "$className <$valueString>"
+}
--- src/test/kotlin/com/baronlior/jke/AssertTest.kt
+++ src/test/kotlin/com/baronlior/jke/AssertTest.kt
@@ -20,9 +20,20 @@ class AssertTest {
   @Test
   fun basic() {
     it("test the simple stuff") {
+
       expect { 2 + 2 toBe 4 }
-      expect("!=") { 2 + 2 notToBe 5 }
-      expect("smaller than") { 2+2 toBeLessThan 1 }
+      expect { 2 + 2 notToBe 5 }
+      expect { 2 + 2 toBeLessThan 11 }
+
+    }
+  }
+
+  @Test
+  fun toBeDeepEqual() {
+    it("toBeDeepEqual") {
+
+      expect("different object references") { arrayOf("1", "2") notToBe arrayOf("1", "2") }
+      expect{ arrayOf(1, "2") toBeDeepEqual arrayOf(1, "2") }
     }
   }
 
